adc53067ebf67c521ddec8771844e6f124dad8f2
Mohammad Aqdas <95559517+aqdas77@users.noreply.github.com>
2023-10-16 20:31:14 +0530

    Update index.jsx

--- client/src/components/Signup/index.jsx
+++ client/src/components/Signup/index.jsx
@@ -9,7 +9,6 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import MuiAlert from '@mui/material/Alert';
 import { useState } from "react";
 import axios from "axios";
 import { Link,useNavigate } from "react-router-dom";
